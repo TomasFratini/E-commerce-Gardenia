5a84f116a0ec604237d07d311191c06aa9ac1ce5
Tomas Fratini <tomas.fratini09@gmail.com>
2022-04-06 23:23:08 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Mi primer app con React.js</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
